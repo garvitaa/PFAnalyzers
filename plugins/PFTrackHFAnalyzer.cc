b76f06b074f2f05d463cd39d5e430cabe56b49bd
garvitaa <38080271+garvitaa@users.noreply.github.com>
2021-01-23 22:32:02 -0500

    Update PFTrackHFAnalyzer.cc

--- plugins/PFTrackHFAnalyzer.cc
+++ plugins/PFTrackHFAnalyzer.cc
@@ -94,7 +94,7 @@ using reco::TrackCollection;
 
 bool mysort(const reco::PFCandidate& elem1, const reco::PFCandidate& elem2){ return elem1.energy() > elem2.energy() ; }
 
-const int ETA_BINS = 82;
+/*const int ETA_BINS = 82;
 
 float etabins[ETA_BINS+1] =
   {-5.191, -4.889, -4.716, -4.538, -4.363, -4.191, -4.013, -3.839, -3.664, -3.489, -3.314, -3.139, -2.964, -2.853, -2.65,
@@ -102,7 +102,11 @@ float etabins[ETA_BINS+1] =
    -0.879, -0.783, -0.696, -0.609, -0.522, -0.435, -0.348, -0.261, -0.174, -0.087, 0,
    0.087, 0.174, 0.261, 0.348, 0.435, 0.522, 0.609, 0.696, 0.783, 0.879, 0.957, 1.044, 1.131, 1.218, 1.305, 1.392, 1.479,
    1.566, 1.653, 1.74, 1.83, 1.93, 2.043, 2.172, 2.322, 2.5, 2.65, 2.853, 2.964, 3.139, 3.314, 3.489, 3.664, 3.839, 4.013,
-   4.191, 4.363, 4.538, 4.716, 4.889, 5.191};
+   4.191, 4.363, 4.538, 4.716, 4.889, 5.191};*/
+const int ETA_BINS = 20;
+float etabins[ETA_BINS+1] =
+ {-5.191, -4.013, -2.964, -2.5, -1.93, -1.566, -1.305, -0.957, -0.696, -0.435, 0,
+  0.435, 0.696, 0.957, 1.305, 1.566, 1.93, 2.5, 2.964, 4.013, 5.191};
 
 class PFTrackHFAnalyzer : public edm::one::EDAnalyzer<edm::one::SharedResources>  {
   public:
@@ -364,11 +368,11 @@ PFTrackHFAnalyzer::PFTrackHFAnalyzer(const edm::ParameterSet& iConfig)
   m_Histos2D[hname] = fs->make<TH2F>(hname, hname , 22 , -1.5 , 20.5, 22 , -1.5 , 20.5 );
 
   hname = "match_ptfrac";
-  m_Histos1D[hname] = fs->make<TH1F>(hname, hname , 500, 0. , 100.0 );   
+  m_Histos1D[hname] = fs->make<TH1F>(hname, hname , 5000, 0. , 100.0 );   
   hname = "match_ptfrac5";
-  m_Histos1D[hname] = fs->make<TH1F>(hname, hname , 500, 0. , 100.0 );
+  m_Histos1D[hname] = fs->make<TH1F>(hname, hname , 5000, 0. , 100.0 );
   hname = "match_ptfrac9";
-  m_Histos1D[hname] = fs->make<TH1F>(hname, hname , 500, 0. , 100.0 );
+  m_Histos1D[hname] = fs->make<TH1F>(hname, hname , 5000, 0. , 100.0 );
 
   hname = "pfclusEmax_frac";
   m_Histos1D[hname] = fs->make<TH1F>(hname, hname , 500, 0. , 100.0 );
@@ -449,40 +453,40 @@ PFTrackHFAnalyzer::PFTrackHFAnalyzer(const edm::ParameterSet& iConfig)
   m_Histos1D[hname] = fs->make<TH1F>(hname, hname , 16 , -0.5, 15.5 );
 
   hname = "Eratio_PFcand1toGen";
-  m_Histos1D[hname] = fs->make<TH1F>(hname, hname , 200 , 0. , 100. );
+  m_Histos1D[hname] = fs->make<TH1F>(hname, hname , 5000 , 0. , 100. );
   hname = "Eratio_PFcand2toGen";
-  m_Histos1D[hname] = fs->make<TH1F>(hname, hname , 200 , 0. , 100. );
+  m_Histos1D[hname] = fs->make<TH1F>(hname, hname , 5000 , 0. , 100. );
   hname = "Eratio_PFcand3toGen";
-  m_Histos1D[hname] = fs->make<TH1F>(hname, hname , 200 , 0. , 100. );
+  m_Histos1D[hname] = fs->make<TH1F>(hname, hname , 5000 , 0. , 100. );
 
   hname = "Eratio_PFcandAlltoGen";
-  m_Histos1D[hname] = fs->make<TH1F>(hname, hname , 200 , 0. , 100. );
+  m_Histos1D[hname] = fs->make<TH1F>(hname, hname , 5000 , 0. , 100. );
   hname = "Eratio_PFcandNonTtoGen";
-  m_Histos1D[hname] = fs->make<TH1F>(hname, hname , 200 , 0. , 100. );
+  m_Histos1D[hname] = fs->make<TH1F>(hname, hname , 5000 , 0. , 100. );
 
   hname = "Eratio_PFcandEM1toGen";  
-  m_Histos1D[hname] = fs->make<TH1F>(hname, hname , 200 , 0. , 100. );
+  m_Histos1D[hname] = fs->make<TH1F>(hname, hname , 5000 , 0. , 100. );
   hname = "Eratio_PFcandHAD1toGen";  
-  m_Histos1D[hname] = fs->make<TH1F>(hname, hname , 200 , 0. , 100. );
+  m_Histos1D[hname] = fs->make<TH1F>(hname, hname , 5000 , 0. , 100. );
   hname = "Eratio_PFcandEMHAD1toGen";
-  m_Histos1D[hname] = fs->make<TH1F>(hname, hname , 200 , 0. , 100. );
+  m_Histos1D[hname] = fs->make<TH1F>(hname, hname , 5000 , 0. , 100. );
 
   hname = "PovGenE_brem";
-  m_Histos1D[hname] = fs->make<TH1F>(hname, hname , 200 , 0. , 100. );
+  m_Histos1D[hname] = fs->make<TH1F>(hname, hname , 5000 , 0. , 100. );
   hname = "PovGenE_nobrem";
-  m_Histos1D[hname] = fs->make<TH1F>(hname, hname , 200 , 0. , 100. );
+  m_Histos1D[hname] = fs->make<TH1F>(hname, hname , 5000 , 0. , 100. );
 
   hname = "EovP";
-  m_Histos1D[hname] = fs->make<TH1F>(hname, hname , 100 , 0. , 5. );
+  m_Histos1D[hname] = fs->make<TH1F>(hname, hname , 1000 , 0. , 5. );
   hname = "EovP_brem";
-  m_Histos1D[hname] = fs->make<TH1F>(hname, hname , 100 , 0. , 5. );
+  m_Histos1D[hname] = fs->make<TH1F>(hname, hname , 1000 , 0. , 5. );
   hname = "EovP_nobrem";
-  m_Histos1D[hname] = fs->make<TH1F>(hname, hname , 100 , 0. , 5. );
+  m_Histos1D[hname] = fs->make<TH1F>(hname, hname , 1000 , 0. , 5. );
 
   hname = "EovPPU_brem";
-  m_Histos1D[hname] = fs->make<TH1F>(hname, hname , 100 , 0. , 5. );
+  m_Histos1D[hname] = fs->make<TH1F>(hname, hname , 1000 , 0. , 5. );
   hname = "EovPPU_nobrem";
-  m_Histos1D[hname] = fs->make<TH1F>(hname, hname , 100 , 0. , 5. );
+  m_Histos1D[hname] = fs->make<TH1F>(hname, hname , 1000 , 0. , 5. );
 
 
 
