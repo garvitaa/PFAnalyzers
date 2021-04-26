e02d5557189f3689dfd083b08b4e80c7ee1924f5
garvitaa <38080271+garvitaa@users.noreply.github.com>
2021-04-27 01:07:35 +0200

    Updated the bins

--- run_miniAOD_singleParticle_plots.py
+++ run_miniAOD_singleParticle_plots.py
@@ -4,7 +4,7 @@ import array
 import numpy as np
 
 etabins = array.array("d",[0.0, 0.5, 1.3, 2.1, 2.5, 3.0, 5.2])
-ptbins = np.arange(0.,20.,0.5)
+ptbins = np.arange(0.,21.,2)
 #[10,24,32,43,56,74,97,133,174,245,300,362,430,507,592,686,846,1032,1248,1588,2000,2500,3000,4000,6000])
 
 def deltaR2( e1, p1, e2=None, p2=None):
@@ -43,23 +43,23 @@ ROOT.AutoLibraryLoader.enable()
 ROOT.gROOT.SetStyle('Plain') # white background
 H_ParticleEta_gen = ROOT.TH1F ("ParticleEta_gen","ParticleEta_gen",int(len(etabins)-1),etabins)
 H_ParticleEta_matched = ROOT.TH1F ("ParticleEta","ParticleEta",int(len(etabins)-1),etabins)
-H_ParticleEta_fake = ROOT.TH1F ("ParticleEta_Fake","ParticleEta_Fake",int(len(etabins)-1),etabins)
+#H_ParticleEta_fake = ROOT.TH1F ("ParticleEta_Fake","ParticleEta_Fake",int(len(etabins)-1),etabins)
 
 H_ParticlePt_gen = ROOT.TH1F ("ParticlePt_gen","ParticlePt_gen",int(len(ptbins)-1),ptbins)
 H_ParticlePt_matched = ROOT.TH1F ("ParticlePt_matched","ParticlePt_matched",int(len(ptbins)-1),ptbins)
-H_ParticlePt_fake = ROOT.TH1F ("ParticlePt_fake","ParticlePt_fake",int(len(ptbins)-1),ptbins)
+#H_ParticlePt_fake = ROOT.TH1F ("ParticlePt_fake","ParticlePt_fake",int(len(ptbins)-1),ptbins)
 
 H_recoJet_eta = ROOT.TH1F ("recoJet_eta","recoJet_eta",int(len(etabins)-1),etabins)
-H_recoJet_eta_matched = ROOT.TH1F ("recoJet_eta_matched","recoJet_eta_matched",int(len(etabins)-1),etabins)
-H_recoJet_eta_fake = ROOT.TH1F ("recoJet_eta_fake","recoJet_eta_fake",int(len(etabins)-1),etabins)
+#H_recoJet_eta_matched = ROOT.TH1F ("recoJet_eta_matched","recoJet_eta_matched",int(len(etabins)-1),etabins)
+#H_recoJet_eta_fake = ROOT.TH1F ("recoJet_eta_fake","recoJet_eta_fake",int(len(etabins)-1),etabins)
 
 H_recoJet_pt = ROOT.TH1F ("recojet_pt","recojet_pt",int(len(ptbins)-1),ptbins)
-H_recoJet_pt_matched = ROOT.TH1F ("recojet_pt_matched","recojet_pt_matched",int(len(ptbins)-1),ptbins)
-H_recoJet_pt_fake = ROOT.TH1F ("recojet_pt_fake","recojet_pt_fake",int(len(ptbins)-1),ptbins)
+#H_recoJet_pt_matched = ROOT.TH1F ("recojet_pt_matched","recojet_pt_matched",int(len(ptbins)-1),ptbins)
+#H_recoJet_pt_fake = ROOT.TH1F ("recojet_pt_fake","recojet_pt_fake",int(len(ptbins)-1),ptbins)
 
 H_recoJetRaw_pt = ROOT.TH1F ("recojetRaw_pt","recojetRaw_pt",int(len(ptbins)-1),ptbins)
-H_recoJetRaw_pt_matched = ROOT.TH1F ("recojetRaw_pt_matched","recojetRaw_pt_matched",int(len(ptbins)-1),ptbins)
-H_recoJetRaw_pt_fake = ROOT.TH1F ("recojetRaw_pt_fake","recojetRaw_pt_fake",int(len(ptbins)-1),ptbins)
+#H_recoJetRaw_pt_matched = ROOT.TH1F ("recojetRaw_pt_matched","recojetRaw_pt_matched",int(len(ptbins)-1),ptbins)
+#H_recoJetRaw_pt_fake = ROOT.TH1F ("recojetRaw_pt_fake","recojetRaw_pt_fake",int(len(ptbins)-1),ptbins)
 
 H_pfcand_eta = ROOT.TH1F ("pfcand_eta","pfcand_eta",int(len(etabins)-1),etabins)
 H_pfcand_eta_matched = ROOT.TH1F ("pfcand_eta_matched","pfcand_eta_matched",int(len(etabins)-1),etabins)
@@ -75,7 +75,7 @@ for i in etabins:
   sub=[]
   for j in ptbins:
     hname = "response_pt"+(str(j).replace('.', 'p'))+"_eta"+(str(i).replace('.', 'p'))
-    sub.append(ROOT.TH1F(hname, hname, 200, 0, 10))
+    sub.append(ROOT.TH1F(hname, hname, 52, 0, 2))
   H_response_pt.append(sub)
 
 # load FWlite python libraries
@@ -118,24 +118,18 @@ for iev,event in enumerate(events):
     H_ParticlePt_gen.Fill(pgenp.pt())
     H_ParticleEta_gen.Fill(abs(pgenp.eta()))
 
-    match = False # matchimg gen particles to pfcands
+    #match = False # matching gen particles to pfcands
     for k,j in enumerate(pfcands.product()):
       if j.pt() < 0:continue
-      if abs(pgenp.pdgId())==211:
-        if deltaR2(pgenp,j)<0.01:
-          match=True
-          recop = j
-    if match:
-      H_ParticlePt_matched.Fill(pgenp.pt())
-      H_ParticleEta_matched.Fill(pgenp.eta())
-      i_eta = int(np.digitize([abs(pgenp.eta())],etabins))-1
-      i_pt = int(np.digitize([pgenp.pt()],ptbins))-1
-      print "Response: %5.3f i_pt: %3i i_eta: %3i" % (recop.pt() / pgenp.pt(), i_pt, i_eta)
-      if (i_eta <len(etabins)) & (i_pt < len(ptbins)):
-        H_response_pt[i_eta][i_pt].Fill(recop.pt() / pgenp.pt())
-    else:
-      H_ParticlePt_fake.Fill(pgenp.pt())
-      H_ParticleEta_fake.Fill(pgenp.eta())
+      if (int(abs(pgenp.pdgId())) == 211) & (int(abs(j.pdgId())) == 211) & (deltaR2(pgenp,j)<0.01):
+        print "matched: run %6d, event %10d, pt %5.1f eta %5.2f pdgId %5d %5.3f %5.3f " % ( event.eventAuxiliary().run(), event.eventAuxiliary().event(),j.pt(), j.eta(), j.pdgId(), j.rawCaloFraction(), j.rawHcalFraction())
+        H_ParticlePt_matched.Fill(pgenp.pt())
+        H_ParticleEta_matched.Fill(pgenp.eta())
+        i_eta = int(np.digitize([abs(pgenp.eta())],etabins))-1
+        i_pt = int(np.digitize([pgenp.pt()],ptbins))-1
+        print "    RecopT: %5.3f GenpT: %5.3f Response: %5.3f i_pt: %3i i_eta: %3i" % (j.pt(), pgenp.pt(), (j.pt() / pgenp.pt()), i_pt, i_eta)
+        if (i_eta <len(etabins)) & (i_pt < len(ptbins)):
+          H_response_pt[i_eta][i_pt].Fill(j.pt() / pgenp.pt())
 
   # Jets (standard AK4)
   for i,j in enumerate(jets.product()):
@@ -146,7 +140,7 @@ for iev,event in enumerate(events):
     H_recoJetRaw_pt.Fill(j.pt()*j.jecFactor('Uncorrected'))
     H_recoJet_eta.Fill(j.eta())
 
-    match = False
+    '''match = False
     for k,pgenp in enumerate(pgenpars.product()):
       if abs(pgenp.pdgId())==211:
         if deltaR2(pgenp,j)<0.01:
@@ -159,7 +153,7 @@ for iev,event in enumerate(events):
     else:
       H_recoJet_pt_fake.Fill(j.pt())
       H_recoJetRaw_pt_fake.Fill(j.pt()*j.jecFactor('Uncorrected'))
-      H_recoJet_eta_fake.Fill(j.eta())
+      H_recoJet_eta_fake.Fill(j.eta())'''
 
   # pfcands
   for i,j in enumerate(pfcands.product()):
@@ -186,23 +180,23 @@ for iev,event in enumerate(events):
 f = ROOT.TFile.Open("myfile.root","RECREATE")
 H_ParticleEta_gen.Write()
 H_ParticleEta_matched.Write()
-H_ParticleEta_fake.Write()
+#H_ParticleEta_fake.Write()
 
 H_ParticlePt_gen.Write()
 H_ParticlePt_matched.Write()
-H_ParticlePt_fake.Write()
+#H_ParticlePt_fake.Write()
 
 H_recoJet_eta.Write()
-H_recoJet_eta_matched.Write()
-H_recoJet_eta_fake.Write()
+#H_recoJet_eta_matched.Write()
+#H_recoJet_eta_fake.Write()
 
 H_recoJet_pt.Write()
-H_recoJet_pt_matched.Write()
-H_recoJet_pt_fake.Write()
+#H_recoJet_pt_matched.Write()
+#H_recoJet_pt_fake.Write()
 
 H_recoJetRaw_pt.Write()
-H_recoJetRaw_pt_matched.Write()
-H_recoJetRaw_pt_fake.Write()
+#H_recoJetRaw_pt_matched.Write()
+#H_recoJetRaw_pt_fake.Write()
 
 H_pfcand_eta.Write()
 H_pfcand_eta_matched.Write()
